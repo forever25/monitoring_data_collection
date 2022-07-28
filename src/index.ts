a7e34ef0da8a67cb736a387761f9a75864d46a42
GY\wangshengz <wangshengz@goyu-ai.com>
2022-07-28 15:20:46 +0800

    feat: 向window抛出入口

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
-import DataCollection from "./modules/DataCollection";
+import PromiseError from "./modules/PromiseError";
+class DataCollection {
+  url: string;
+  token: string;
+  params: ModelProps;
+  constructor(props: DataCollectionProps) {
+    this.token = props.token;
+    this.url = props.url;
+    this.params = {
+      url: this.url,
+      token: this.token,
+    };
+    this.init(this.params);
+  }
+
+  init(params: ModelProps) {
+    new PromiseError(params);
+  }
+}
+
 window.DataCollection = DataCollection;
