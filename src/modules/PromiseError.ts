d47907fce11a0b0024ea93afab62d203539d939d
GY\wangshengz <wangshengz@goyu-ai.com>
2022-08-04 13:56:30 +0800

    feat: 添加title和浏览器版本字段

--- src/modules/PromiseError.ts
+++ src/modules/PromiseError.ts
@@ -14,11 +14,13 @@ export default class PromiseError extends BaseModel {
     window.addEventListener("unhandledrejection", (event) => {
       try {
         this.add("promiseError", {
+          title: document.title,
           errorId: createErrorId(),
           msg: event.reason,
           type: event.type,
           url: window.location.href,
           timeStamp: new Date().getTime(),
+          userAgent: navigator.userAgent,
         });
         event.preventDefault();
       } catch (error) {
