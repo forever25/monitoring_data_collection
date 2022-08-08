2234beb152431bce22b6c177be7a229ed33d7cdd
GY\wangshengz <wangshengz@goyu-ai.com>
2022-08-08 11:44:00 +0800

    test: 测试用例更改

--- src/modules/PromiseError.ts
+++ src/modules/PromiseError.ts
@@ -11,22 +11,26 @@ export default class PromiseError extends BaseModel {
    * @return {*}
    */
   init(): void {
-    window.addEventListener("unhandledrejection", (event) => {
-      try {
-        this.add("promiseError", {
-          title: document.title,
-          errorId: createErrorId(),
-          msg: event.reason,
-          type: event.type,
-          url: window.location.href,
-          timeStamp: new Date().getTime(),
-          userAgent: navigator.userAgent,
-        });
-        event.preventDefault();
-      } catch (error) {
-        console.error(error);
-      }
-      this.sync(); // 测试同步功能
-    });
+    window.addEventListener(
+      "unhandledrejection",
+      (event) => {
+        try {
+          this.add("promiseError", {
+            title: document.title,
+            errorId: createErrorId(),
+            msg: event.reason,
+            type: event.type,
+            url: window.location.href,
+            timeStamp: new Date().getTime(),
+            userAgent: navigator.userAgent,
+          });
+          event.preventDefault();
+        } catch (error) {
+          console.error(error);
+        }
+        this.sync(); // 测试同步功能
+      },
+      true
+    );
   }
 }
